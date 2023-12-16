eaa97a865df2992115d98e048ef8d6a91ee45b38
RyuNumchon <127880773+RyuNumchon@users.noreply.github.com>
2023-12-16 10:59:59 +0000

    change2

--- src/dashboard.py
+++ src/dashboard.py
@@ -14,14 +14,14 @@ st.text("gan with 3 disentangled features")
 
 if st.button('Generate'):
     random_idx = random.randint(1, 2)
-    num_features = 3
-    
-    # st.image(f'/mount/src/spatialattganx/src/outputs/disentangled_features/{num_features}/output_{random_idx}.png')
-    
+    num_features = 3  
+
     input_path = f'/mount/src/spatialattganx/src/outputs/disentangled_features/{num_features}/input_{random_idx}.png'
     output_path = f'/mount/src/spatialattganx/src/outputs/disentangled_features/{num_features}/output_{random_idx}.png'
+    
     st.text("Original Image from GAN")
     st.image(input_path)
+    
     st.text("GAN with Multiple Distangled features ['Grey hair', 'Red Lips', 'Mustache']")
     st.text(f"Total: {num_features} disentangled features")
     st.image(output_path)
